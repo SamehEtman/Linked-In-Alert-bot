5b5ab5c79996609801481d5084b892868472d31a
Sameh <samehothman20162017@gmail.com>
2021-10-23 16:15:10 +0200

    wire yargs library

--- api/linkedInJobs.js
+++ api/linkedInJobs.js
@@ -1,11 +1,18 @@
 const request = require('request-promise');
 const _ = require('lodash');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
 
+try {
+  if (!argv.title) throw 'Please add Title';
+  if (!argv.location) throw 'Please add Title';
+} catch (error) {
+  console.log(error);
+  process.exit();
+}
 
-const options = require('./jobRequestOptions')(
-  process.argv[2] || 'node js',
-  process.argv[3] || 'Egypt'
-);
+const options = require('./jobRequestOptions')(argv.title, argv.location);
 
 const extractJobs = (included, jobs = []) => {
   jobs = included.filter((item) => item.dashEntityUrn);
@@ -14,19 +21,23 @@ const extractJobs = (included, jobs = []) => {
 };
 
 module.exports = async (oldJobs) => {
-  const { included } = JSON.parse(await request(options));
-  const allJobs = extractJobs(included);
-  let size;
-  for (size = 0; size < allJobs.length; size++) {
-    if (_.isEqual(allJobs[size], oldJobs[0])) {
-      break;
+  try {
+    const { included } = JSON.parse(await request(options));
+    const allJobs = extractJobs(included);
+    let size;
+    for (size = 0; size < allJobs.length; size++) {
+      if (_.isEqual(allJobs[size], oldJobs[0])) {
+        break;
+      }
     }
-  }
-  let newJobs = [];
-  for (let i = 0; i < size; i++) {
-    newJobs.push(allJobs[i]);
-  }
-  const jobsUrl = options.headers.Referer;
+    let newJobs = [];
+    for (let i = 0; i < size; i++) {
+      newJobs.push(allJobs[i]);
+    }
+    const jobsUrl = options.headers.Referer;
 
-  return { allJobs, newJobs, size, jobsUrl };
+    return { allJobs, newJobs, size, jobsUrl };
+  } catch (error) {
+    throw new Error(error);
+  }
 };
